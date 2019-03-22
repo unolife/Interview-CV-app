22cd016aae8eaabc3bae8b892cd24332e664a8a3
sbs <unolife92@gmail.com>
2019-03-22 09:58:49 +0900

    frist commit, makes two button and link them to each activity

---
+++ app/src/main/java/com/cookandroid/interview_cv_app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.cookandroid.interview_cv_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_cv, btn_iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_cv = (Button) findViewById(R.id.button_CV);
+        btn_iv = (Button) findViewById(R.id.button_IV);
+
+        btn_cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Intent = new Intent(MainActivity.this, CVActivity.class);
+                startActivity(Intent);
+            }
+        });
+
+        btn_iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Intent = new Intent(MainActivity.this, IVActivity.class);
+                startActivity(Intent);
+            }
+        });
+    }
+}
