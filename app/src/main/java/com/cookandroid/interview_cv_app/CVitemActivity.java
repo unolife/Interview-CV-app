08e50746c0fb3a57b739c279e3a44c3c36b8a672
unolife92 <unolife92@gmail.com>
2019-03-23 22:21:59 +0900

    show cname and img in CVitem

--- app/src/main/java/com/cookandroid/interview_cv_app/CVitemActivity.java
+++ app/src/main/java/com/cookandroid/interview_cv_app/CVitemActivity.java
@@ -2,8 +2,14 @@ package com.cookandroid.interview_cv_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CVitemActivity extends AppCompatActivity {
+
+    ImageView ivPicture;
+    TextView tvCname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +17,10 @@ public class CVitemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cvitem);
         CV cv = (CV) getIntent().getSerializableExtra("CV");
 
+        ivPicture = findViewById(R.id.iv_picture);
+        tvCname = findViewById(R.id.tv_cname);
+
+        ivPicture.setImageResource(cv.getDrawableId());
+        tvCname.setText(cv.getCname().toString());
     }
 }
